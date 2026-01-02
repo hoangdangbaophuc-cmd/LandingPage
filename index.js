964083dff98988c93583fc09897544990b4374ab
hoangdangbaophuc-cmd <hoangdangbaophuc@gmail.com>
2026-01-02 12:28:58 +0700

    LandingPageTest

---
+++ index.js
@@ -0,0 +1,15 @@
+
+
+
+// document.addEventListener('DOMContentLoaded',function(){
+//     const cards = document.querySelectorAll(".cards")
+
+//     cards.forEach(element=>{
+//         element.addEventListener('mouseenter',function(){
+//             this.classList.add('hover')
+//         })
+//         element.addEventListener('mouseleave', function(){
+//             this.classList.remove('hover')
+//         })
+//     })
+// })
